facb56a5f6274d1673eb817a05259bb09778c4f0
samih freire <samihcisne@gmail.com>
2025-10-02 10:06:14 -0300

    feat: criado e estilizado o componente de letter

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./app.module.css"
 
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
+import { Letter } from "./components/Letter"
 
 export default function App() {
 
@@ -15,6 +16,14 @@ export default function App() {
         <Header current={5} max={10} onRestart={handleRestartGame} />
         
         <Tip tip="Uma das linguagem de programação mais utilizadas"/>
+
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
       </main>
     </div>
   )
