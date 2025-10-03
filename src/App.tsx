c1acd190bcdc1fb72253168f05e4102b3578d05e
samih freire <samihcisne@gmail.com>
2025-10-03 10:28:46 -0300

    feat: criado a handleConfirm

--- src/App.tsx
+++ src/App.tsx
@@ -30,11 +30,33 @@ export default function App() {
     setLetter("");
   }
 
+  function handleConfirm() {
+    if(!challenge) {
+      return;
+    }
+
+    // Caso usuário nao tenha digitado uma letra
+    if(!letter.trim()) {
+      return alert("Digite uma letra!");
+    }
+
+    const value = letter.toUpperCase();
+    const exists = lettersUsed.find((used) => used.value.toUpperCase() === value);
+    
+    if(exists) {
+      return alert("Você já utilizou a letra " + value);
+    }
+
+    setLettersUsed((prevState) => [...prevState, { value, correct: false }]);
+    
+    setLetter("");
+  }
+
   useEffect(() => {
     startGame()
   }, [])
 
-  if(!challenge){
+  if(!challenge) {
     return;
   }
 
@@ -57,8 +79,15 @@ export default function App() {
         <h4>Palpite</h4>
 
         <div className={styles.guess}>
-          <Input autoFocus maxLength={1} placeholder="?" />
-          <Button title="Confirmar"/>
+          <Input 
+            autoFocus 
+            maxLength={1} 
+            placeholder="?" 
+            value={letter}
+            onChange={(e) => setLetter(e.target.value)}
+          />
+
+          <Button title="Confirmar" onClick={handleConfirm}/>
         </div>
 
         <LettersUsed data={lettersUsed} />
